6fdd5ce4e4262622a95262589e9b0c5443fb47a8
CarlosGonzalez1280 <70284946+CarlosGonzalez1280@users.noreply.github.com>
2021-11-28 16:14:17 -0500

    A ton of stuff was done
    A ton of stuff was done

--- App.js
+++ App.js
@@ -1,62 +1,16 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Pressable,
-  useWindowDimensions,
-} from 'react-native';
-import Card from './src/components/Card';
-import users from './assets/data/users';
-import Animated, { interpolate, useDerivedValue, useSharedValue, useAnimatedStyle, withSpring, useAnimatedGestureHandler } from 'react-native-reanimated';
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import { StyleSheet, View} from 'react-native';
 
-const ROTATION = 60; 
+import HomeScreen from './src/screens/HomeScreen';
+import MatchesScreen from './src/screens/MatchesScreen';
+
 
 const App = () => {
-  
-  const {width: screenWidth} = useWindowDimensions();
-  const hiddenTranslateX = 2 * screenWidth;
-  const translateX = useSharedValue(0);
-  const rotate = useDerivedValue(() => interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION], ) + 'deg');
-  const cardStyle = useAnimatedStyle(() =>({
-    transform: [
-        {
-            translateX: translateX.value,
-        },
-        {
-            rotate: rotate.value,
-        }
-    ],
-
-  }));
-
-  const gestureHandler = useAnimatedGestureHandler({
-      onStart:(event, context ) => {
-          context.startX = translateX.value;
-      },
-      onActive: (event, context) => {
-          translateX.value = context.startX + event.translationX;
-      },
-      onEnd:() => {
-          console.log('Touch Ended');
-      },
-  });
-
   return (
     <View style = {styles.pageContainer}>
-        <PanGestureHandler onGestureEvent = {gestureHandler}>
-        <Animated.View style = {[styles.animatedCard, cardStyle]}>
-        <Card user = {users[0]}/>
-      </Animated.View>
-      </PanGestureHandler>
-      <Pressable 
-        onPress={() => (translateX.value = withSpring(Math.random() ))}>
-          <Text>CHANGE VALUE</Text>
-        </Pressable>
+      <MatchesScreen />
     </View>
- 
   );
 };
 
@@ -66,11 +20,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
   },
-  animatedCard:{
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
 });
 
 
---
+++ users.js
@@ -0,0 +1,31 @@
+export default [
+    {
+      id: '1',
+      name: 'Luis Cruz',
+      image:
+        require('./assets/pexels-photo-946337.jpeg'),
+
+      bio: 'Random Thing Here',
+    },
+    {
+      id: '2',
+      name: 'Lawrence Ballard',
+      image:
+        require('./assets/pexels-photo-2968723.jpeg'),
+      bio: 'bio',
+    },
+    {
+      id: '3',
+      name: 'Seth Riley',
+      image:
+        require('./assets/pexels-photo-4046276.jpeg'),
+      bio: 'bio',
+    },
+    {
+      id: '4',
+      name: 'Marlene Elliott',
+      image:
+        require('./assets/pexels-pixabay-461593.jpg'),
+      bio: 'Hola',
+    },
+  ];
