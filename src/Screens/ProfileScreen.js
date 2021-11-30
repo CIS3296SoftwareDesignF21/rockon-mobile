cdff108a32bbb83e671a67be18e61d2fb6640f8f
CarlosGonzalez1280 <70284946+CarlosGonzalez1280@users.noreply.github.com>
2021-11-29 22:42:31 -0500

    New Simple Screens

---
+++ src/Screens/ProfileScreen.js
@@ -0,0 +1,65 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Pressable,
+  TextInput,
+  Alert,
+} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+
+const ProfileScreen = () => {
+    return (
+        <SafeAreaView style={styles.root}>
+          <View style={styles.container}>
+            <TextInput
+              style={styles.input}
+              placeholder="Name..."
+            />
+    
+            <TextInput
+              style={styles.input}
+              placeholder="bio..."
+              multiline
+              numberOfLines={3}
+            />
+    
+            <Pressable onPress={() => console.warn("Profile Edited")} style={styles.button}>
+              <Text>Save</Text>
+            </Pressable>
+    
+            <Pressable onPress={() => console.warn('Profile Signed Out')} style={styles.button}>
+              <Text>Sign out</Text>
+            </Pressable>
+          </View>
+        </SafeAreaView>
+      );
+}
+
+const styles = StyleSheet.create({
+    root: {
+      width: '100%',
+      flex: 1,
+      padding: 10,
+    },
+    container: {
+      padding: 10,
+    },
+    button: {
+      backgroundColor: '#F63A6E',
+      height: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 20,
+      margin: 10,
+    },
+    input: {
+      margin: 10,
+      borderBottomColor: 'lightgray',
+      borderBottomWidth: 1,
+    },
+  });
+
+export default ProfileScreen;
--- users.js
+++ users.js
@@ -3,7 +3,7 @@ export default [
       id: '1',
       name: 'Luis Cruz',
       image:
-        require('./assets/pexels-photo-946337.jpeg'),
+        require('./assets/Images/pexels-photo-946337.jpeg'),
 
       bio: 'Random Thing Here',
     },
@@ -11,21 +11,42 @@ export default [
       id: '2',
       name: 'Lawrence Ballard',
       image:
-        require('./assets/pexels-photo-2968723-2.jpeg'),
+        require('./assets/Images/pexels-photo-2968723-2.jpeg'),
       bio: 'bio',
     },
     {
       id: '3',
       name: 'Seth Riley',
       image:
-        require('./assets/pexels-photo-4046276.jpeg'),
+        require('./assets/Images/pexels-photo-4046276.jpeg'),
       bio: 'bio',
     },
     {
       id: '4',
       name: 'Marlene Elliott',
       image:
-        require('./assets/pexels-pixabay-461593.jpg'),
+        require('./assets/Images/pexels-pixabay-461593.jpg'),
       bio: 'Hola',
     },
+    {
+        id: '5',
+        name: 'Alina Oliveras',
+        image:
+            require('./assets/Images/pexels-photo-7591335.jpeg'),
+        bio: "Hello people",
+    },
+    {
+        id: '6',
+        name: 'Leila Contreras',
+        image:
+            require('./assets/Images/pexels-photo-4009355.jpeg'),
+        bio: "Whats up",
+    },
+    {
+        id: '7',
+        name: 'Tyler Wolff',
+        image:
+            require('./assets/Images/pexels-photo-6008052.jpeg'),
+        bio: 'Whats up',
+    }
   ];
